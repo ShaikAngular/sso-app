c744017385c5e6b4517ce315727b59805b1d0227
Shaik <indivarangular@gmail.com>
2022-04-07 17:03:39 +0530

    SSO initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { authConfig } from './sso.config';
+import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
+//import { filter } from 'minimatch';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sso-app';
+  constructor(private oauthService:OAuthService){
+    this.configureSingleSignOn();
+  }
+  configureSingleSignOn(){
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+   // this.oauthService.loadDiscoveryDocumentAndTryLogin();
+   this.oauthService.loadDiscoveryDocumentAndLogin();
+  }
+
+  // login(){
+  //   this.oauthService.initImplicitFlow();
+  // }
+  
+  logout(){
+    this.oauthService.logOut();
+  }
+
+  get token(){
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
+
+  
 }
