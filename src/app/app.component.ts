59c71ac3071a59f163656840ca31ec5292c101d9
Shaik <indivarangular@gmail.com>
2022-04-12 16:43:16 +0530

    sso logout from local, and logout from SSO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { OAuthService } from 'angular-oauth2-oidc';
 import { authConfig } from './sso.config';
 import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
 //import { filter } from 'minimatch';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -17,16 +18,50 @@ export class AppComponent {
   configureSingleSignOn(){
     this.oauthService.configure(authConfig);
     this.oauthService.tokenValidationHandler = new JwksValidationHandler();
-   // this.oauthService.loadDiscoveryDocumentAndTryLogin();
-   this.oauthService.loadDiscoveryDocumentAndLogin();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+   //this.oauthService.loadDiscoveryDocumentAndLogin();
   }
 
-  // login(){
-  //   this.oauthService.initImplicitFlow();
-  // }
+  login(){
+    this.oauthService.initImplicitFlow();
+    
+  }
   
   logout(){
-    this.oauthService.logOut();
+    swal(
+      {
+    title: 'Do you want to logout?',
+    
+   showCancelButton:true,
+   cancelButtonText:'Logout from SSO',
+   showCloseButton:true,
+   showConfirmButton:true,
+   confirmButtonText:'Logout from Local',
+   showLoaderOnConfirm:true
+     
+     }).then(result=>
+      {
+        if(result.dismiss){
+          this.oauthService.logOut();
+        }
+        if(result.value){
+          sessionStorage.clear();
+          document.location.href = window.location.origin + '/index.html'
+        }
+        console.log("result",result)
+      }
+       
+     )
+    
+    
+    // sessionStorage.clear();
+    // document.location.href = window.location.origin + '/index.html'
+    //this.oauthService.logOut();
+    //document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://www.example.com";
+    console.log("token",this.token)
+  
+    //this.oauthService.logOut();
+    //console.log("logout", this.oauthService.logOut())
   }
 
   get token(){
